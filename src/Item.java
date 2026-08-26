7455ab1a5b627c8f2576736f76b28d0d5f9febef
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-08-26 08:07:51 -0300

    Create Item.java
    No mesmo padrão de Personagem pra ficar mais fácil de corrigir

---
+++ src/Item.java
@@ -0,0 +1,42 @@
+public class Item {
+    private String item_nome;
+    private String item_tipo;
+    private String item_descricao;
+    private int item_id;
+    private int item_quant;
+
+    public Item(String item_nome, String item_tipo, String item_descricao, int item_id, int item_quant) {
+        this.item_nome = item_nome;
+        this.item_tipo = item_tipo;
+        this.item_descricao = item_descricao;
+        this.item_id = item_id;
+        this.item_quant = item_quant;
+    }
+
+    public String getNome() {
+        return this.item_nome;
+    }
+
+    public int getQuant() {
+        return this.item_quant;
+    }
+
+    public String getDescricao() {
+        return this.item_descricao;
+    }
+
+    public void Consumir(int quantidade) {
+        if (item_quant > quantidade) {
+            item_quant -= quantidade;
+            System.out.println("Você Consumiu: " + getNome());
+        } else {
+            System.out.println("Você não tem " + getNome() + "suficientes para isso!");
+        }
+    }
+
+    public void Incrementar(int quantidade) {
+        //if(item<0) tem que criar o item aq? ou só pode incrementar se já existir o item no inventário
+        item_quant += quantidade;
+        System.out.println("Você coleutou " + quantidade + getNome());
+    }
+}

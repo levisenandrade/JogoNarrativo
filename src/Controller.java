8f0de217d31e812c96aef2bc1ce44c7245212046
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-08-25 22:44:52 -0300

    Create Controller.java

---
+++ src/Controller.java
@@ -0,0 +1,7 @@
+public class Controller {
+    public static void main(String[] args) {
+        Jogo save1 = new Jogo();
+        save1.novoJogo();//Iniciei um jogo novo (Criação do personagem+ prologo)
+        //Primeiro Cenário+escolhas
+    }
+}

f396b6e6ffd54cb981af6436c0f6cd046c8526fd
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-08-25 22:44:07 -0300

    Integração do Controlador
    Adicionei o método Cria Personagem pra criar o mc no método criajogo do controlador

--- src/Personagem.java
+++ src/Personagem.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Personagem {
     private String nome;
     private int idade;
@@ -5,29 +6,49 @@ public class Personagem {
     private String raca;
     private int afinidade;
 
-     public Personagem(String nome,  int idade, String sexo, String raca, int afinidade) {
+    public Personagem(String nome,  int idade, String sexo, String raca, int afinidade) {
 
-         this.nome = nome;
-         this.idade = idade;
-         this.sexo = sexo;
-         this.raca = raca;
-         this.afinidade = afinidade;
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.raca = raca;
+        this.afinidade = afinidade;
 
-     }
+    }
+    public Personagem criaPersonagem() {
+        Scanner scanner = new Scanner(System.in);
 
-     public String getNome() {
-         return this.nome;
-     }
+        System.out.println("Digite o nome do seu Personagem:");
+        String novo_nome = scanner.nextLine();
 
-     public int getAfinidade() {
-         return this.afinidade;
-     }
+        System.out.println("Digite sua idade:");
+        int nova_idade = scanner.nextInt();
 
-     public void aumentaAfinidade(int quantidade) {
-         this.afinidade += quantidade;
-     }
-    
-     public void reduzAfinidade(int quantidade) {
-         this.afinidade -= quantidade;
-     }
+        System.out.println("Digite o sexo do personagem:");
+        String novo_sexo = scanner.nextLine();
+
+        System.out.println("Digite a raça do personagem:");
+        String novo_raca = scanner.nextLine();
+
+        System.out.println("Digite a afinidade do personagem (número):");
+        int novo_afinidade = scanner.nextInt();
+        scanner.nextLine(); // Consome a quebra de linha pendente
+
+        return new Personagem(novo_nome, nova_idade, novo_sexo, novo_raca, novo_afinidade);
+    }
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getAfinidade() {
+        return this.afinidade;
+    }
+
+    public void aumentaAfinidade(int quantidade) {
+        this.afinidade += quantidade;
+    }
+
+    public void reduzAfinidade(int quantidade) {
+        this.afinidade -= quantidade;
+    }
 }

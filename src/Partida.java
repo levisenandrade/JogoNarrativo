95e3082793304c73b0c0dc7ae1563f933886c102
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-08-28 15:27:57 -0300

    Update and rename Controller.java to Partida.java

--- src/Partida.java
+++ src/Partida.java
@@ -1,6 +1,6 @@
-public class Controller {
+public class Partida {
     public static void main(String[] args) {
-        Jogo save1 = new Jogo();
+        Controlador save1 = new Jogo();
         save1.novoJogo();//Iniciei um jogo novo (Criação do personagem+ prologo)
         //Primeiro Cenário+escolhas
     }

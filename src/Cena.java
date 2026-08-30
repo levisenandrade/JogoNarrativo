09cff399124ee9ad03e1426069c58e46236188a3
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-08-30 15:47:36 -0300

    Update Cena.java

--- src/Cena.java
+++ src/Cena.java
@@ -1,30 +1,67 @@
 public class Cena {
-    // Classe Cena recebe apenas o texto da cena e as opções disponíveis
-    // visto que eventualmente uma consequência pode ser mais que só mostrar a mensagem
+    private Dialogo[] arrayDialogos;
+    private int dialogoAtual;
 
-    private String texto; // diálogo principal
-    private String opcao1; // opção 1
-    private String opcao2; // opção 2
-    private String opcao3; // opção 3
-    private Menu menu;
-
-    public Cena(String texto, String op1, String op2, String op3, Menu menu) {
-        // depois ver como faço pra colocar valores default na escolha, ou seja, strings em branco
-        this.texto = texto;
-        this.opcao1 = op1;
-        this.opcao2 = op2;
-        this.opcao3 = op3;
-        this.menu = menu;
+    //Construtor com Tamanho padrão de dialogos
+    public Cena() {
+        this.arrayDialogos = new Dialogo[5];
+        this.dialogoAtual = 0;
     }
 
-    public int executarCena() {
-
-        System.out.println("\n" + this.texto + "\n");
-        System.out.println("1 - " + this.opcao1 + "\n");
-        System.out.println("2 - " + this.opcao2 + "\n");
-        System.out.println("3 - " + this.opcao3 + "\n");
-
-        return menu.validaOpcao();
-
+    //Construtor para quantiades maiores de dialogos
+    public Cena(int quantidadeDialogos) {
+        this.arrayDialogos = new Dialogo[quantidadeDialogos];
+        this.dialogoAtual = 0;
     }
+
+
+    //Criação de Dialogo que possui somente Texto
+    public void adicionarDialgoSemOpcoes(String texto) {
+        if (dialogoAtual < arrayDialogos.length) {
+            arrayDialogos[dialogoAtual] = new Dialogo(texto);
+            dialogoAtual += 1;
+        } else {
+            System.out.println("Limite de dialogos atingido para esse turno!!!");
+        }
+    }
+
+    //Criação de Dialogo que possui texto, opçções e consequências diretas
+        //As consequências de efeitos já são aplicadas dentro do metodo
+    public void adicionaDialogoComOpcoes(Personagem mc, String texto, String op1, String op2, String op3, String[] atributos, int[] valores) {
+        if (dialogoAtual < arrayDialogos.length) {
+            arrayDialogos[dialogoAtual] = new Dialogo(texto, op1, op2, op3,atributos,valores);
+
+            String atributoResultante = arrayDialogos[dialogoAtual].getAtributo();
+            int valorResultante = arrayDialogos[dialogoAtual].getValor();
+
+            dialogoAtual += 1;
+        } else {
+            System.out.println("Limite de dialogos atingido para esse turno!!!");
+        }
+    }
+
+
+    public void executaDialogos(Personagem mc) {
+        for (int i = 0; i < dialogoAtual; i++) {
+            Dialogo d = arrayDialogos[i];
+            d.executarDialogo();
+
+            // Só aplica efeitos se o diálogo possuir opções
+            if (d.possuiOpcoes()) {
+
+                int valorEfeito = d.getValor();
+                String atributoNome = d.getAtributo();
+
+                aplicaEfeitos(mc, atributoNome, valorEfeito);
+            }
+        }
+    }
+    public void aplicaEfeitos(Personagem personagem, String nomeAtributo, int valor) {
+        if (personagem != null) {
+            personagem.alteraAtributo(nomeAtributo, valor);
+        } else {
+            System.out.println("Erro: Personagem inválido.");
+        }
+    }
+
 }

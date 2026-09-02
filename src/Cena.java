ae27019240b43d49286ce8e7395ca2e6056668be
Levi <levisenandrade@gmail.com>
2026-09-02 20:32:40 -0300

    refatora Cena para gerenciar diálogos
    refatora Cena para receber Entrada, armazenar diálogos e permitir a criação de diálogos com ou sem opções e também altera a execução para percorrer e executar os diálogos armazenados na cena

--- src/Cena.java
+++ src/Cena.java
@@ -1,67 +1,101 @@
 public class Cena {
+
     private Dialogo[] arrayDialogos;
     private int dialogoAtual;
+    private Entrada entrada;
 
-    //Construtor com Tamanho padrão de dialogos
-    public Cena() {
+    // Construtor com tamanho padrão de diálogos
+    public Cena(Entrada entrada) {
         this.arrayDialogos = new Dialogo[5];
         this.dialogoAtual = 0;
+        this.entrada = entrada;
     }
 
-    //Construtor para quantiades maiores de dialogos
-    public Cena(int quantidadeDialogos) {
+    // Construtor para quantidades maiores de diálogos
+    public Cena(int quantidadeDialogos, Entrada entrada) {
         this.arrayDialogos = new Dialogo[quantidadeDialogos];
         this.dialogoAtual = 0;
+        this.entrada = entrada;
     }
 
+    // Criação de diálogo que possui somente texto
+    public void adicionaDialogoSemOpcoes(
+            Personagem personagem,
+            String texto
+    ) {
 
-    //Criação de Dialogo que possui somente Texto
-    public void adicionaDialogoSemOpcoes(String texto) {
         if (dialogoAtual < arrayDialogos.length) {
-            arrayDialogos[dialogoAtual] = new Dialogo(texto);
+
+            arrayDialogos[dialogoAtual] =
+                    new Dialogo(
+                            personagem,
+                            texto,
+                            entrada
+                    );
+
             dialogoAtual += 1;
+
         } else {
-            System.out.println("Limite de dialogos atingido para esse turno!!!");
+            System.out.println(
+                    "Limite de dialogos atingido para esse turno!!!"
+            );
         }
     }
 
-    //Criação de Dialogo que possui texto, opçções e consequências diretas
-        //As consequências de efeitos já são aplicadas dentro do metodo
-    public void adicionaDialogoComOpcoes(Personagem mc, String texto, String op1, String op2, String op3, String[] atributos, int[] valores) {
-        if (dialogoAtual < arrayDialogos.length) {
-            arrayDialogos[dialogoAtual] = new Dialogo(texto, op1, op2, op3,atributos,valores);
+    // Criação de diálogo que possui texto, opções e consequências
+    public void adicionaDialogoComOpcoes(
+            Personagem mc,
+            String texto,
+            String op1,
+            String op2,
+            String op3,
+            String[] atributos,
+            int[] valores
+    ) {
 
-            String atributoResultante = arrayDialogos[dialogoAtual].getAtributo();
-            int valorResultante = arrayDialogos[dialogoAtual].getValor();
+        if (dialogoAtual < arrayDialogos.length) {
+
+            arrayDialogos[dialogoAtual] =
+                    new Dialogo(
+                            mc,
+                            texto,
+                            op1,
+                            op2,
+                            op3,
+                            atributos,
+                            valores,
+                            entrada
+                    );
 
             dialogoAtual += 1;
+
         } else {
-            System.out.println("Limite de dialogos atingido para esse turno!!!");
+            System.out.println(
+                    "Limite de dialogos atingido para esse turno!!!"
+            );
         }
     }
 
+    public void executaDialogos() {
 
-    public void executaDialogos(Personagem mc) {
         for (int i = 0; i < dialogoAtual; i++) {
+
             Dialogo d = arrayDialogos[i];
+
             d.executarDialogo();
-
-            // Só aplica efeitos se o diálogo possuir opções
-            if (d.possuiOpcoes()) {
-
-                int valorEfeito = d.getValor();
-                String atributoNome = d.getAtributo();
-
-                aplicaEfeitos(mc, atributoNome, valorEfeito);
-            }
         }
     }
-    public void aplicaEfeitos(Personagem personagem, String nomeAtributo, int valor) {
-        if (personagem != null) {
-            personagem.alteraAtributo(nomeAtributo, valor);
+
+    public void aplicaEfeitos(
+            Protagonista protagonista,
+            String nomeAtributo,
+            int valor
+    ) {
+
+        if (protagonista != null) {
+            protagonista.alteraAtributo(nomeAtributo, valor);
         } else {
             System.out.println("Erro: Personagem inválido.");
         }
     }
-
 }

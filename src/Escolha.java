cfd5319246c7b7c3f6ca2cc481d1055db8de3908
Levi <levisenandrade@gmail.com>
2026-09-03 20:12:50 -0300

    criação da classe escolha

---
+++ src/Escolha.java
@@ -0,0 +1,12 @@
+public class Escolha {
+
+    private String texto;
+
+    public Escolha(String texto) {
+        this.texto = texto;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+}

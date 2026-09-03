85553a59cbf16dea886fb280d2465c39e0a56153
Levi <levisenandrade@gmail.com>
2026-09-03 20:54:14 -0300

    método getAtributo adicionado e mudança nos valores iniciais
    mudei os valroes iniciais que eram 0 para 50 também, igual ao atributo "confiança" dos NPCs

--- src/Protagonista.java
+++ src/Protagonista.java
@@ -6,9 +6,9 @@ public class Protagonista extends Personagem {
 
         atributos = new Atributo[3];
 
-        atributos[0] = new Atributo("Razão", 0, 100);
-        atributos[1] = new Atributo("Paranoia", 0, 100);
-        atributos[2] = new Atributo("Violencia", 0, 100);
+        atributos[0] = new Atributo("Razão", 50, 100);
+        atributos[1] = new Atributo("Paranoia", 50, 100);
+        atributos[2] = new Atributo("Violencia", 50, 100);
     }
 
     public void alteraAtributo(String nomeAtributo, int quantidade) {
@@ -22,8 +22,15 @@ public class Protagonista extends Personagem {
                 return;
             }
         }
-      
         System.out.println("Atributo não existente");
     }
 
+    public int getAtributo(String nomeAtributo) {
+        for (int i = 0; i < atributos.length; i++) {
+            if (atributos[i].getNome().equalsIgnoreCase(nomeAtributo)) {
+                return atributos[i].getValor();
+            }
+        }
+        return 0;
+    }
 }

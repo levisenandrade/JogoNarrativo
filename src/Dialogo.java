42e370d7ce2b251e72ce11a42fe51ab87cfd5085
Levi <levisenandrade@gmail.com>
2026-09-04 19:14:32 -0300

    remove antigo método de manipulação dos atributos, agora é tudo feito pela classe Escolha
    remove os getters antigos e protótipoi do metodo que permitia alterar os atributos afetados pela escolha

--- src/Dialogo.java
+++ src/Dialogo.java
@@ -2,42 +2,32 @@ public class Dialogo {
 
     private Personagem personagem;
     private String texto;
-    private String opcao1;
-    private String opcao2;
-    private String opcao3;
-
-    private String[] arrayDeAtributosAfetados;
-    private int[] arrayDeValoresCorrespondentes;
+    private Escolha[] escolhas;
 
     // Construtor de diálogo sem opções
     public Dialogo(Personagem personagem, String texto) {
         this.personagem = personagem;
         this.texto = texto;
 
-        this.opcao1 = null;
-        this.opcao2 = null;
-        this.opcao3 = null;
+        this.escolhas = null;
     }
 
     // Construtor de diálogo com opções
     public Dialogo(
             Personagem personagem,
             String texto,
-            String opcao1,
-            String opcao2,
-            String opcao3,
-            String[] atributos,
-            int[] valores
+            Escolha escolha1,
+            Escolha escolha2,
+            Escolha escolha3
     ) {
         this.personagem = personagem;
         this.texto = texto;
 
-        this.opcao1 = opcao1;
-        this.opcao2 = opcao2;
-        this.opcao3 = opcao3;
+        this.escolhas = new Escolha[3];
 
-        this.arrayDeAtributosAfetados = atributos;
-        this.arrayDeValoresCorrespondentes = valores;
+        this.escolhas[0] = escolha1;
+        this.escolhas[1] = escolha2;
+        this.escolhas[2] = escolha3;
     }
 
     public void executarDialogo() {
@@ -50,33 +40,17 @@ public class Dialogo {
         System.out.println(texto);
 
         if (possuiOpcoes()) {
-            System.out.println(opcao1);
-            System.out.println(opcao2);
-            System.out.println(opcao3);
+            for (Escolha escolha : escolhas) {
+                System.out.println(escolha.getTexto());
+            }
         }
     }
 
     public boolean possuiOpcoes() {
-        return opcao1 != null;
+        return escolhas != null;
     }
 
-    public String getOpcao1() {
-        return opcao1;
-    }
-
-    public String getOpcao2() {
-        return opcao2;
-    }
-
-    public String getOpcao3() {
-        return opcao3;
-    }
-
-    public String[] getArrayDeAtributosAfetados() {
-        return arrayDeAtributosAfetados;
-    }
-
-    public int[] getArrayDeValoresCorrespondentes() {
-        return arrayDeValoresCorrespondentes;
+    public Escolha getEscolha(int indice) {
+        return escolhas[indice - 1];
     }
 }
